cb7944d30aaf91c56f4e25e553e90d1bb0e2652b
korpan-d-24 <korpan24denis@gmail.com>
2022-01-29 00:01:11 +0200

    First connect server and client

---
+++ ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ChatClient.MVVM.Core;
+using ChatClient.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public RelayCommand ConnectToServerComand { get; set; }
+
+        private Server _server;
+
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerComand = new RelayCommand(o => _server.ConnectToServer());
+        }
+    }
+}
